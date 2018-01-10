a0430cea7f2e09e5857929a39c26f155002a58c8
Tito <rabelo51@gmail.com>
2018-01-10 16:58:04 -0300

    pequena alteraçao em settings.py para AWS s3

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -219,7 +219,11 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = 'titoecommerce'
 AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'djangoecommerce.s3util.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'djangoecommerce.s3util.MediaStorage'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 AWS_HEADERS = {
     'x-amz-acl': 'public-read',
