b3fe08280e07e0d941e7e0fa339634b9b408fa3d
Tito <rabelo51@gmail.com>
2018-01-10 16:40:42 -0300

    settings.py and local_settings.py AWS S3

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -206,6 +206,26 @@ PAGSEGURO_SANDBOX = True
 PAYPAL_TEST = True
 PAYPAL_EMAIL = "rabelo51@gmail.com"
 
+# AWS (Amazon Web Services - S3)
+
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
+AWS_S3_SECURE_URLS = True
+AWS_QUERYSTRING_AUTH = False
+AWS_PRELOAD_METADATA = True
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '') 
+AWS_STORAGE_BUCKET_NAME = 'titoecommerce'
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_HEADERS = {
+    'x-amz-acl': 'public-read',
+    'Cache-Control': 'public, max-age=31556926',
+}
+
 # An alias is a specific set of thumbnail options.
 THUMBNAIL_ALIASES = {
     '': {
